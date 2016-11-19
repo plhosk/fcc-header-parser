b902e56c0f0df05e795a6c3c12d25191c95682bb
Paul Hoskinson <plhosk@gmail.com>
2016-11-19 15:20:20 -0700

    user story fulfilled

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require(express)
+require('babel-polyfill')
+const createLocaleMiddleware = require('express-locale')
+const useragent = require('express-useragent')
+const express = require('express')
 const app = express()
 
 app.set('port', (process.env.PORT ||  5000))
 
+app.use(createLocaleMiddleware())
+app.use(useragent.express())
+
 app.get('*', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(req)
+  //res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.send({
+    ipaddress: req.ip,
+    language: req.locale.code,
+    software: req.useragent.source
+  })
 })
 
 app.listen(app.get('port'), () => {
